0a42bc802af85775a104601591c75820ad006bf2
marcosmolinamaxicon <marcos.molina@maxiconsystems.com.br>
2019-04-29 09:57:38 -0300

    link solic

--- app/slashcommands-solic/client/client.js
+++ app/slashcommands-solic/client/client.js
@@ -2,14 +2,13 @@
 import { Meteor } from 'meteor/meteor';
 import { slashCommands } from '../../utils';
 
-function Solic(command, params /* , item*/) {
+function Solic(command, params, item) {
 	console.log('solic', command);
 	console.log('params', params);
 	if (command === 'solic') {
-		const msg = {};
-
-		//  msg.msg = params + '['+item+']'+'(https://sds.maxiconsystems.com.br/pls/maxicon/show_solic_v2?i_nr_solicitacao='+item+')';
-		console.log('solic', msg);
+		const url = `https://sds.maxiconsystems.com.br/pls/maxicon/show_solic_v2?i_nr_solicitacao=${ params }`;
+		const msg = item;
+		msg.msg = `${ url }`;
 		Meteor.call('sendMessage', msg);
 	}
 }
